75cc9f24280b47ff95d8c8bd2931cded69756904
Ian W. Remmel <design@ianwremmel.com>
2016-12-22 13:44:52 -0800

    chore(tooling): use common eslint rules

--- src/format-package.js
+++ src/format-package.js
@@ -2,12 +2,8 @@ const _ = require(`lodash/fp`);
 const fs = require(`fs`);
 const config = require(`./config`);
 
-// TODO figure out github repository
-// TODO default engines to current node/npm version
-// TODO require user.name
-// TODO require user.email
-
 module.exports = function formatPackage(packagePath) {
+  // eslint-disable-next-line global-require
   const pkg = require(packagePath);
   let result = Object.assign({}, config.get(`format-package:defaults`), pkg, config.get(`format-package:overrides`));
   result = config.get(`format-package:order`).reduce(sorter, {});
